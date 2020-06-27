f06f9f6b68ac65eef04a99845d89eb46e3aa717e
Bastien <bastien.ratat@gmail.com>
2020-06-27 19:30:46 +0200

    create icons and play with different settings for the map

--- src/components/request/Map.js
+++ src/components/request/Map.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import logo from '../../images/logo.png';
 import mapStyles from "./mapStyles";
 import {
     GoogleMap,
@@ -22,6 +23,8 @@ const center = {
 
 const options = {
     styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true
 };
 
 export const MapComponent = () => {
@@ -30,17 +33,42 @@ export const MapComponent = () => {
         libraries,
     });
 
+    const [markers, setMarkers] = React.useState([]);
+
     if (loadError) return "Error loading Maps";
     if (!isLoaded) return "Loading Maps"
     return (
         <Container className="d-flex justify-content-center">
             <div>
+                <h1 className="map-sub-title">LOVE & HELP</h1>
                 <GoogleMap
                     mapContainerStyle={mapContainerStyle}
-                    zoom={8}
+                    zoom={12}
                     center={center}
                     options={options}
-                />
+                    onClick={(event) => {
+                        setMarkers((current) =>  [
+                            ...current,
+                            {
+                                lat: event.latLng.lat(),
+                                lng: event.latLng.lng(),
+                                time: new Date()
+                        }])
+                    }}
+                >
+                    {markers.map((marker) => (
+                        <Marker
+                            key={marker.time.toISOString()}
+                            position={{ lat: marker.lat, lng: marker.lng }}
+                            icon={{
+                                url: logo,
+                                scaledSize: new window.google.maps.Size(50,50),
+                                origin: new window.google.maps.Point(0,0),
+                                anchor: new window.google.maps.Point(25,25)
+                            }}
+                        />
+                    ))}
+                </GoogleMap>
             </div>
         </Container>
     )
