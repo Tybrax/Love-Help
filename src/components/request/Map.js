952c8afcc6fb58ce8cecf91f99de2f27b0bb9d1e
Bastien <bastien.ratat@gmail.com>
2020-06-27 13:47:39 +0200

    implement maps with 2020 syntax using hooks

--- src/components/request/Map.js
+++ src/components/request/Map.js
@@ -1,27 +1,49 @@
 import React from 'react';
-import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps'
+import { Container } from 'react-bootstrap';
+import mapStyles from "./mapStyles";
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow,
+} from '@react-google-maps/api'
 
-const Map = () => {
-    return (
-        <GoogleMap
-            defaultZoom={10}
-            defaultCenter={{ lat:43.294640, lng: 5.368660 }}
-        />
-    );
-}
+const libraries = ["places"];
 
-const WrappedMap = withGoogleMap(Map);
+const mapContainerStyle = {
+    width: '100vh',
+    height:'100vh'
+};
+
+const center = {
+    lat: 43.294640 ,
+    lng: 5.368660
+};
+
+const options = {
+    styles: mapStyles,
+};
 
 export const MapComponent = () => {
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        libraries,
+    });
+
+    if (loadError) return "Error loading Maps";
+    if (!isLoaded) return "Loading Maps"
     return (
-        <div className="mx-auto" style={{width: '100vh', height: '100vh'}}>
-            <WrappedMap
-                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `100%` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            />
-        </div>
-    );
+        <Container className="d-flex justify-content-center">
+            <div>
+                <GoogleMap
+                    mapContainerStyle={mapContainerStyle}
+                    zoom={8}
+                    center={center}
+                    options={options}
+                />
+            </div>
+        </Container>
+    )
 }
 
+export default MapComponent;
