04dc5dca8ba603dc3d5e3ba7a4db3108a42e2de9
Bastien <bastien.ratat@gmail.com>
2020-07-17 11:12:30 +0200

    count component makes a get request and return the number of requests every 0.5s

--- src/components/homepage/Count.js
+++ src/components/homepage/Count.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Container } from 'react-bootstrap';
+
+const requestEndPoint = 'http://localhost:3001/api/v1/requests'
 
 const Counter = (props) => {
 
-    const [request, newRequest] = useState(0);
+    const [count, newCount] = useState(0);
 
     useEffect(() => {
         setTimeout(() => {
-            /*GET request with Axios to refresh the counter of requests*/
-        }, 2000);
+            axios.get(requestEndPoint)
+            .then(res => {
+                newCount(res.data.length)
+            })
+            .catch(e => {
+                console.log(e)
+            })
+        }, 500);
     })
 
     return (
         <Container fluid className='count d-flex mx-auto align-items-center justify-content-center'>
-            <h3 className='text-center'>Unfulfilled request ({ props.date }) : { request }</h3>
+            <h3 className='text-center'>Unfulfilled request ({ props.date }) : { count }</h3>
         </Container>
     )
 };
